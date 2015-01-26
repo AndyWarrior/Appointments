76bd3bfa8dfcdbf37e8ad1c7cd1a9b41d01fd837
Andres De Leon <agldeleon@gmail.com>
2015-01-25 20:49:09 -0600

    Add seeds to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+	physicians = Physician.create([{ name: 'Dr. Juan Gomez'}, { name: 'Dr. Octavio Seguro'}, { name: 'Dr. Ignacio Rodriguez'}])
+	patients = Patient.create([{ name: 'Andrea Camacho'}, { name: 'Maria Lozano'}, { name: 'Joaquin Ramirez'}])
